ff3fdb184d1021a0b2aae627348167ae83ae3895
willy-Jules <79364676+willy-Jules@users.noreply.github.com>
2025-11-11 13:39:39 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_willy-Jules_chipbootcamp (
+module tt_um_WillyJules_chipbootcamp (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
