8bab129ba275701adb37eac655791be43dc8de4f
willy-Jules <79364676+willy-Jules@users.noreply.github.com>
2025-11-11 13:48:22 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_WilyJules_chip(
+  tt_um_WilyJules_chip user_project(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
